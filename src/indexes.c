a5a643b7570af6ffb4850320dc9f09640c92431a
Savel Suopea <ssuopea@student.hive.fi>
2025-08-23 11:57:46 +0300

    added getters for x and y from index

--- src/indexes.c
+++ src/indexes.c
@@ -6,7 +6,7 @@
 /*   By: ssuopea <ssuopea@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 14:21:03 by ssuopea           #+#    #+#             */
-/*   Updated: 2025/08/22 14:23:49 by ssuopea          ###   ########.fr       */
+/*   Updated: 2025/08/23 11:57:08 by ssuopea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,14 @@ int	i(int x, int y)
 int	center(void)
 {
 	return(i(WIDTH / 2, HEIGHT / 2));
+}
+
+int	x(int i)
+{
+	return (i % WIDTH);
+}
+
+int y(int i)
+{
+	return (i / WIDTH);
 }
