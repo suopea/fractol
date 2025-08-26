d3e784ebf8f154e1e20daf5d0a860cf4cba9d38a
suopea <suopea@pm.me>
2025-08-26 19:29:51 +0300

    refactored color

--- src/draw.c
+++ src/draw.c
@@ -17,18 +17,18 @@ void	clear_screen(t_data *data)
 	memset(data->frame->pixels, 0, data->px_count * sizeof(int32_t));
 }
 
-static	int32_t	normal(int escape_time, int iterations)
-{
-	// return (((iterations / escape_time % 16) * 0x111111) << 8 | 0xFF);
-	// return ((((iterations - escape_time + 100) % 0xFF) * 0x010101) << 8 | 0xFF);
-	return ((((iterations - escape_time + 30) % 0x88) * 0x020202) << 8 | 0xFF);
-	// return (((((iterations - escape_time + 100) * 20) % 0xFF) * 0x010101) << 8 | 0xFF);
-}
+// static	int32_t	normal(int escape_time, int iterations)
+// {
+// 	// return (((iterations / escape_time % 16) * 0x111111) << 8 | 0xFF);
+// 	// return ((((iterations - escape_time + 100) % 0xFF) * 0x010101) << 8 | 0xFF);
+// 	return ((((iterations - escape_time + 30) % 0x88) * 0x020202) << 8 | 0xFF);
+// 	// return (((((iterations - escape_time + 100) * 20) % 0xFF) * 0x010101) << 8 | 0xFF);
+// }
 
 void	colorize_pixels(t_data *data)
 {
 	int	i;
-	int	normalized;
+	int	color;
 
 	i = 0;
 	while (i < data->px_count)
@@ -37,8 +37,8 @@ void	colorize_pixels(t_data *data)
 			mlx_put_pixel(data->frame, x(i, data), y(i, data), 0xFF);
 		else
 		{
-			normalized = normal(data->escape_times[i], data->iteration);
-			mlx_put_pixel(data->frame, x(i, data), y(i, data), normalized);
+			color = (((data->iteration - data->escape_times[i] + 30) % 0x88) * 0x020202) << 8 | 0xFF;
+			mlx_put_pixel(data->frame, x(i, data), y(i, data), color);
 		}	
 		i++;
 	}
